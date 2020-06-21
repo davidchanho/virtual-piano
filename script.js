d990ba6f7d15cb1b9df88baa4fe5658477a808f0
davidchanho <davidho.crd@gmail.com>
2020-06-21 19:27:54 -0400

    finish

---
+++ script.js
@@ -0,0 +1,11 @@
+const keys = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'w', 'e', 't', 'y', 'u']
+
+document.addEventListener('keydown', function (event) {
+    keys.forEach(key => {
+        if (event.key === key) {
+            console.log(key)
+            const audio = new Audio(`assets/${key.toUpperCase()}.mp3`);
+            audio.play()
+        }
+    })
+})
